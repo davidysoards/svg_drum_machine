f2585ed5ce39a456f716da49d2eb5146addffdfb
David Soards <ninjasoards@Davids-MacBook-Pro.local>
2018-01-31 20:04:39 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const pad = document.querySelector(`.pad[data-key="${e.keyCode}"]`);
+  if(!audio) return; // stop function if no audio
+  audio.currentTime = 0; //rewind to start
+  audio.play();
+  pad.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; //skip it
+  this.classList.remove('playing');
+}
+
+const pads =  document.querySelectorAll('.pad');
+pads.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// pads.forEach(key => key.addEventListener('click', playSound));
+// document.onClick = playSound;
+// document.addEventListener('click', playSound);
